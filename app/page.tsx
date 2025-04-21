5156a0b2ff709e0360169a35d30d6a200b5e274b
flowz0 <billylflowers@gmail.com>
2025-04-21 09:16:01 -0700

    fix: remove Statistics component implementation until the component is complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "./components/home/Hero";
-import Statistics from "./components/home/Statistics";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Statistics />
     </>
   );
 };
