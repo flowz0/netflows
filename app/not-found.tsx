ee5a8638ca947b8be2855dd04c7f88a5f46e850e
flowz0 <billylflowers@gmail.com>
2025-04-20 17:16:11 -0700

    fix: update css for consistency

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function PageNotFound() {
   return (
-    <div className="bg-[#FBFBFF] text-[#040F16] h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold">Page not found</h1>
-      <Link 
-        href="/" 
-        className="bg-[#01BAEF] text-[#FBFBFF] mt-6 font-bold px-4 py-2 rounded-md hover:bg-[hsl(193,99%,37%)]"
+    <div className="bg-[#040F16] text-[#FBFBFF] h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold">Page not found</h1>
+      <Link
+        href="/"
+        className="bg-[#01BAEF] text-[#FBFBFF] mt-8 font-bold px-4 py-2 rounded-md hover:bg-[hsl(193,99%,37%)]"
       >
-        Go back
+        Return to home
       </Link>
     </div>
-  )
-}
+  );
+};
