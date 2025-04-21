43ca282c2c8de5616b4fcb7f2775cacd3b43f92b
flowz0 <billylflowers@gmail.com>
2025-04-21 09:11:27 -0700

    fix: optimize Hero component and implement Button component

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,19 +1,15 @@
+import Button from "../Button";
 import ReviewsTag from "./ReviewsTag";
 
 export default function Hero() {
   return (
-    <header className="bg-[#040F16] text-[#FBFBFF] h-screen flex flex-col sm:items-center justify-center px-6">
-      <h1 className="text-5xl/[1.10] sm:text-6xl/[1.10] max-w-xs sm:max-w-none font-bold sm:text-center">Creative Digital Agency</h1>
-      <p className="text-[rgba(250,250,255,0.7)] max-w-sm sm:max-w-md text-base/[1.6] sm:text-lg/[1.6] sm:text-center mt-4">
+    <header className="bg-[#040F16] text-[#FBFBFF] h-screen flex flex-col justify-center px-6 sm:items-center">
+      <h1 className="max-w-xs text-5xl/[58px] font-bold sm:max-w-none sm:text-6xl/[70px] sm:text-center">Creative Digital Agency</h1>
+      <p className="text-[rgba(250,250,255,0.7)] max-w-sm text-base/[30px] mt-4 sm:max-w-xl sm:text-lg/[32px] sm:text-center">
         Web development agency based in California. Focused on creating interactive web experiences for small businesses and enterprises.
       </p>
-      <button
-        type="button"
-        className="bg-[#01BAEF] text-[#040F16] hover:bg-[hsl(193,99%,37%)] w-fit mt-6 py-2 px-4 text-lg font-bold rounded-lg cursor-pointer"
-      >
-        View our work
-      </button>
+      <Button className="w-fit mt-6">View our work</Button>
       <ReviewsTag />
     </header>
-  )
+  );
 };
