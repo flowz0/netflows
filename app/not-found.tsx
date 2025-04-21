c5f617a02f4073a1a05790b77c6ff24f67be8016
flowz0 <billylflowers@gmail.com>
2025-04-21 09:16:38 -0700

    fix: optimize not-found page and implement Button component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
+import Button from "./components/Button";
 
 export default function PageNotFound() {
   return (
     <div className="bg-[#040F16] text-[#FBFBFF] h-screen flex flex-col justify-center items-center">
-      <h1 className="text-5xl font-bold">Page not found</h1>
-      <Link
-        href="/"
-        className="bg-[#01BAEF] text-[#FBFBFF] mt-8 font-bold px-4 py-2 rounded-md hover:bg-[hsl(193,99%,37%)]"
-      >
-        Return to home
+      <h1 className="max-w-xs text-4xl/[46px] font-bold text-center sm:max-w-none sm:text-5xl/[58px]">
+        Page not found
+      </h1>
+      <p className="text-[rgba(250,250,255,0.7)] mt-2 max-w-sm text-base/[30px] text-center sm:max-w-xl sm:text-lg/[32px]">
+        This page does not exist.
+      </p>
+      <Link href='/' className="mt-6">
+        <Button>
+          Return to the homepage
+        </Button>
       </Link>
     </div>
   );
