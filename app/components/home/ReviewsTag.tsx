90eb6edae0b147291d8a79c9f296aaeacae644e3
flowz0 <billylflowers@gmail.com>
2025-04-26 18:36:34 -0700

    fix: optimize css on ReviewsTag component

--- app/components/home/ReviewsTag.tsx
+++ app/components/home/ReviewsTag.tsx
@@ -2,17 +2,17 @@ import { FaStar } from "react-icons/fa";
 
 export default function ReviewsTag() {
   return (
-    <div className="bg-[hsl(203,69%,5%)] text-[rgba(250,250,255,0.7)]">
-      <div className="flex items-center gap-x-2.5">
-        <p className="text-[1rem]/[32px] font-bold tracking-[0.016em]">5.0</p>
-        <div className="flex gap-x-0.5">
-          <FaStar className="text-[#01BAEF] w-[1rem] h-[1rem]" />
-          <FaStar className="text-[#01BAEF] w-[1rem] h-[1rem]" />
-          <FaStar className="text-[#01BAEF] w-[1rem] h-[1rem]" />
-          <FaStar className="text-[#01BAEF] w-[1rem] h-[1rem]" />
-          <FaStar className="text-[#01BAEF] w-[1rem] h-[1rem]" />
+    <div className="bg-[hsl(203,69%,8%)] text-[rgba(250,250,255,1)] ring ring-[rgba(250,250,255,0.5)] py-1 px-4 rounded-full">
+      <div className="flex items-center gap-x-4">
+        <p className="text-[1.25rem]/[34px] tracking-[0.016em]">5.0</p>
+        <div className="flex gap-x-1">
+          <FaStar className="text-amber-300 w-4 h-4" />
+          <FaStar className="text-amber-300 w-4 h-4" />
+          <FaStar className="text-amber-300 w-4 h-4" />
+          <FaStar className="text-amber-300 w-4 h-4" />
+          <FaStar className="text-amber-300 w-4 h-4" />
         </div>
-        <p className="text-[1rem]/[30px] tracking-[0.016em]">
+        <p className="text-[rgba(250,250,255,0.7)] text-[1.0rem]/[34px] tracking-[0.016em]">
           (129 Reviews)
         </p>
       </div>
