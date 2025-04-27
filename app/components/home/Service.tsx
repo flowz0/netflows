558d4f5d671955ca84ce9500aedefbfe44fe7f3a
flowz0 <billylflowers@gmail.com>
2025-04-26 18:36:47 -0700

    feat: create Service component and Services section

--- app/components/home/Service.tsx
+++ app/components/home/Service.tsx
@@ -13,10 +13,10 @@ export default function Service({ title, img }: ServiceProps) {
       <Image
         src={img}
         alt="Service image"
-        className="object-cover h-64 w-full rounded-xl border border-[rgba(4,15,22,1.0)] shadow shadow-[#040F16]/70 sm:h-52"
+        className="object-cover h-64 w-full rounded-xl border border-[rgba(4,15,22,1.0)] shadow shadow-[#040F16]/70 sm:h-60"
         draggable={false}
       />
-      <div className="bg-[#040F16] shadow shadow-[#040F16]/70 flex items-center gap-x-4 rounded-xl -translate-y-6 py-4 px-5">
+      <div className="bg-[#040F16] shadow shadow-[#040F16]/70 flex gap-x-4 rounded-xl -translate-y-6 py-4 px-5">
         <h3 className="text-[rgba(251,251,255,0.7)] group-hover:text-[#FBFBFF] duration-300 transition-colors font-bold">{title}</h3>
         <FaLongArrowAltRight className="text-[rgba(251,251,255,0.7)] h-6 w-6 duration-300 transition-transform trnasition-colors group-hover:text-[#01BAEF] group-hover:translate-x-2" />
       </div>
