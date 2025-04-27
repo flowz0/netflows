ca69cad8174d82107e779e876d58a7d467a42ab3
flowz0 <billylflowers@gmail.com>
2025-04-26 18:36:17 -0700

    fix: optimize css on Hero section

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -6,7 +6,7 @@ import LayeredWaves from "@/public/layered-waves.svg";
 
 export default function Hero() {
   return (
-    <header className="bg-[#040F16] text-[#FBFBFF] relative flex justify-center pb-52 pt-28 px-6 sm:pb-60 sm:pt-28">
+    <header className="bg-[#040F16] text-[#FBFBFF] relative flex justify-center h-screen pb-52 pt-28 px-6 sm:pb-60 sm:pt-36">
       <Image
         src={LayeredWaves}
         alt="Blob background"
@@ -19,9 +19,8 @@ export default function Hero() {
         <h1 className="text-4xl/[46px] font-bold tracking-[-0.031em] max-w-lg mt-4 sm:mt-8 sm:text-5xl/[58px] text-center">
         We Build Websites That Grow Your Business
         </h1>
-        <p className="text-[rgba(250,250,255,0.7)] text-base/[30px] tracking-[0.016em] mt-2 sm:mt-4 sm:text-lg/[32px] text-center">
-          Strategic web design and development that turns visitors into
-          customers.
+        <p className="text-[rgba(250,250,255,0.7)] text-base/[30px] tracking-[0.016em] mt-2 max-w-sm sm:mt-4 sm:text-lg/[32px] text-center">
+          Strategic web design and development that turns visitors into customers.
         </p>
         <Button className="w-fit mt-4 sm:mt-8">Book a Free Consultation</Button>
       </div>
