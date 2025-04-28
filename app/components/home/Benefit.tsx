4f4e203282bcca3c5cb03dbff62bc58ef3f9df8a
flowz0 <billylflowers@gmail.com>
2025-04-28 11:09:41 -0700

    fix: optimize Benefit component and update Benefits

--- app/components/home/Benefit.tsx
+++ app/components/home/Benefit.tsx
@@ -7,11 +7,11 @@ interface BenefitProps {
 export default function BenefitCard({ icon: Icon, title, desc }: BenefitProps) {
   return (
     <div className="text-[#040F16] flex flex-col items-center w-56">
-      <div className="bg-[rgba(154,232,254,0.5)] p-5 rounded-full">
-        <Icon className="text-[hsl(193,99%,20%)] h-12 w-12" />
+      <div className="bg-[rgba(154,232,254,0.5)] p-4 rounded-full">
+        <Icon className="text-[hsl(193,99%,20%)] h-6 w-6" />
       </div>
       <div className="flex flex-col items-center justify-center">
-        <h2 className="text-[#040F16] text-3xl/[30px] font-bold tracking-[-0.031em] mt-4">
+        <h2 className="text-[#040F16] text-3xl/[30px] font-bold tracking-[-0.031em] mt-5">
           {title}
         </h2>
         <p className="text-[rgba(4,15,22,0.7)] text-base/[30px] tracking-[0.016em] mt-2">
--- app/components/home/Benefits.tsx
+++ app/components/home/Benefits.tsx
@@ -5,10 +5,10 @@ export default function Benefits() {
   return (
     <div className="bg-[#FBFBFF] flex justify-center px-6 py-8 sm:py-32">
       <div className="max-w-6xl">
-        <div className="flex flex-wrap justify-center gap-y-7 gap-x-4">
-          <Stat icon={FaRocket} title="54" desc="Projects Launched" />
-          <Stat icon={FaStar} title="7+" desc="Years of Experience" />
-          <Stat icon={FaSmile} title="99%" desc="Customer Satisfaction" />
+        <div className="flex flex-wrap justify-center gap-y-9 gap-x-8">
+          <Stat icon={FaRocket} title="54" desc="Projects launched" />
+          <Stat icon={FaStar} title="7" desc="Years of experience" />
+          <Stat icon={FaSmile} title="200%" desc="Customer conversion rate" />
         </div>
       </div>
     </div>
