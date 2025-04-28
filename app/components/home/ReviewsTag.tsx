6eac6ce7a822447b07badd525bf35fe4921ec952
flowz0 <billylflowers@gmail.com>
2025-04-28 11:06:03 -0700

    fix: optimize css on ReviewsTag component

--- app/components/home/ReviewsTag.tsx
+++ app/components/home/ReviewsTag.tsx
@@ -2,9 +2,9 @@ import { FaStar } from "react-icons/fa";
 
 export default function ReviewsTag() {
   return (
-    <div className="bg-[hsl(203,69%,8%)] text-[rgba(250,250,255,1)] ring ring-[rgba(250,250,255,0.5)] py-1 px-4 rounded-full">
+    <div className="bg-[hsl(203,69%,8%)] ring ring-[rgba(250,250,255,0.5)] py-1 px-4 rounded-full">
       <div className="flex items-center gap-x-2">
-        <p className="text-[1.25rem]/[34px] tracking-[0.016em]">5.0</p>
+        <p className="text-[rgba(250,250,255,0.7)] text-[1.125rem]/[32px] tracking-[0.016em]">100+</p>
         <div className="flex gap-x-0.5">
           <FaStar className="text-amber-300 w-4 h-4" />
           <FaStar className="text-amber-300 w-4 h-4" />
@@ -13,7 +13,7 @@ export default function ReviewsTag() {
           <FaStar className="text-amber-300 w-4 h-4" />
         </div>
         <p className="text-[rgba(250,250,255,0.7)] text-[1rem]/[34px] tracking-[0.016em]">
-          (129 Reviews)
+          reviews
         </p>
       </div>
     </div>
