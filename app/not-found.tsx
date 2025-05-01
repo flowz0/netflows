4db1fb89f771322848098a2fce3e9031d126ca1d
flowz0 <billylflowers@gmail.com>
2025-05-01 02:50:26 -0700

    fix: optimize CSS & UX on not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,19 +6,19 @@ import NotFound404 from "@/public/404-not-found.svg";
 
 export default function PageNotFound() {
   return (
-    <section className="py-8 px-6 flex flex-col items-center justify-between max-w-7xl mx-auto lg:flex-row lg:py-32">
-      <div className="flex flex-col order-2">
+    <section className="pt-28 px-6 flex flex-col justify-between max-w-7xl mx-auto lg:flex-row lg:items-center lg:py-32">
+      <div className="flex flex-col">
         <h1 className="text-4xl/[2.75rem] font-bold font-(family-name:--font-roboto-sans) sm:text-5xl/[3.54rem]">
           This Page
           <br />
           Does Not Exist
         </h1>
-        <p className="font-(family-name:--font-inter-sans) text-[#595454] text-base/[2rem] max-w-sm mt-4">
+        <p className="font-(family-name:--font-inter-sans) text-[#595454] leading-[1.85rem] sm:leading-[2rem] max-w-sm mt-4">
           Looks like you&apos;ve hit a dead end. But don&apos;t worry — you can head back home and keep exploring.
         </p>
-        <Link href="/" className="mt-9">
-          <Button type="button">
-            Back to Home
+        <Link href="/" className="mt-9 w-full sm:w-fit">
+          <Button type="button" className="w-full sm:w-fit">
+            Go to homepage
           </Button>
         </Link>
       </div>
@@ -26,7 +26,7 @@ export default function PageNotFound() {
       <Image
         src={NotFound404}
         alt="Page not found image"
-        className="animate-float transition-transform h-full w-96 order-1 mt-8 lg:mt-0 lg:order-2"
+        className="animate-float transition-transform h-full w-96 lg:max-w-lg mt-12 lg:mt-0"
         draggable="false"
       />
     </section>
