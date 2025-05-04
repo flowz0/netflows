1f35190eec236a9b1bbc112c374048d13494da54
flowz0 <billylflowers@gmail.com>
2025-05-04 14:46:59 -0700

    fix: optimize page not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,31 +4,34 @@ import Link from "next/link";
 
 import NotFound404 from "@/public/404-not-found.svg";
 
+export const metadata = {
+  title: "Page Not Found - Netflows"
+};
+
 export default function PageNotFound() {
   return (
-    <section className="pt-28 px-6 flex flex-col justify-between max-w-7xl mx-auto lg:flex-row lg:items-center lg:py-32">
-      <div className="flex flex-col">
-        <h1 className="text-4xl/[2.75rem] font-bold font-(family-name:--font-roboto-sans) sm:text-5xl/[3.54rem]">
-          This Page
-          <br />
-          Does Not Exist
-        </h1>
-        <p className="font-(family-name:--font-inter-sans) text-[#595454] leading-[1.85rem] sm:leading-[2rem] max-w-sm mt-4">
-          Looks like you&apos;ve hit a dead end. But don&apos;t worry — you can head back home and keep exploring.
-        </p>
-        <Link href="/" className="mt-9 w-full sm:w-fit">
-          <Button type="button" className="w-full sm:w-fit">
-            Go to homepage
-          </Button>
-        </Link>
+    <header className="py-28">
+      <div className="max-w-7xl mx-auto px-6 flex flex-col justify-between gap-8 lg:items-center md:flex-row">
+        <div className="max-w-xl">
+          <h1 className="text-[#252323] text-4xl/[2.666rem] font-bold md:text-5xl/[3.516rem]">
+            This Page
+            <br />
+            Does Not Exist
+          </h1>
+          <p className="text-[rgba(37,35,35,0.8)] text-base/[2.024rem] mt-4 md:text-lg/[2.374rem]">
+            Looks like you&apos;ve hit a dead end. But don&apos;t worry — you can head back home and keep exploring.
+          </p>
+          <Link href='/'>
+            <Button className="mt-8 w-full md:w-fit">Go to homepage</Button>
+          </Link>
+        </div>
+        <Image
+          src={NotFound404}
+          alt="Landing page hero image"
+          className="w-auto h-96 object-contain animate-float transition-transform"
+          draggable={false}
+        />
       </div>
-
-      <Image
-        src={NotFound404}
-        alt="Page not found image"
-        className="animate-float transition-transform h-full w-96 lg:max-w-lg mt-12 lg:mt-0"
-        draggable="false"
-      />
-    </section>
+    </header>
   );
 };
