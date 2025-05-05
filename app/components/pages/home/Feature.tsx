3ebb47dc030524d88d6491e7da91915462205d2a
flowz0 <billylflowers@gmail.com>
2025-05-05 12:37:17 -0700

    feat: add iconColor, descTextColor, borderClass props for style customization

--- app/components/pages/home/Feature.tsx
+++ app/components/pages/home/Feature.tsx
@@ -5,18 +5,21 @@ interface FeatureProps {
   title: string;
   desc: string;
   className?: string;
+  iconColor?: string;
+  descTextColor?: string;
+  borderClass?: string;
 }
 
-export default function Feature({ Icon, title, desc, className }: FeatureProps) {
+export default function Feature({ Icon, title, desc, className, iconColor, descTextColor, borderClass }: FeatureProps) {
   return (
-    <div className={`${className} bg-[#252323] rounded-2xl w-full py-7 px-6`}>
+    <div className={`${className} ${borderClass} rounded-2xl w-full pt-8 pb-6 px-5`}>
       <div>
-        <Icon className="w-6 h-6 text-[#EFEFEF]" />
+        <Icon className={`w-6 h-6 ${iconColor}`} />
       </div>
-      <h3 className="text-[#EFEFEF] text-2xl font-semibold mt-5 md:text-3xl">
+      <h3 className={`${className} text-xl font-semibold mt-5 md:text-2xl/[2rem]`}>
         {title}
       </h3>
-      <p className="text-[rgba(239,239,239,0.8)] mt-2">
+      <p className={`${descTextColor} leading-[1.8rem] mt-2`}>
         {desc}
       </p>
     </div>
--- app/components/pages/home/Features.tsx
+++ app/components/pages/home/Features.tsx
@@ -5,26 +5,42 @@ import { FaBolt, FaLock, FaChartLine, FaShoppingCart } from "react-icons/fa";
 export default function Features() {
   return (
     <section>
-      <div className="max-w-7xl px-6 mx-auto grid lg:justify-evenly gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <div className="max-w-7xl pb-32 px-6 mx-auto grid lg:justify-evenly gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <Feature
           title="Lightning-Fast Experiences"
           desc="Optimized websites and apps that load in seconds to keep users engaged and reduce bounce rates."
           Icon={FaBolt}
+          iconColor="text-yellow-400"
+          className="bg-yellow-100 text-yellow-800"
+          descTextColor="text-yellow-800/60"
+          borderClass="border-2 border-yellow-800/60 shadow shadow-yellow-800/20"
         />
         <Feature
           title="Enterprise-Level Protection"
           desc="Safeguard your business with secure architecture, encryption, and proactive monitoring."
           Icon={FaLock}
+          iconColor="text-sky-500"
+          className="bg-sky-100 text-sky-800"
+          descTextColor="text-sky-800/60"
+          borderClass="border-2 border-sky-800/60 shadow shadow-sky-800/20"
         />
         <Feature
           title="Ready to Grow With You"
           desc="Built with flexible infrastructure to support your growth—no matter the size or stage of your business."
           Icon={FaChartLine}
+          iconColor="text-purple-500"
+          className="bg-purple-100 text-purple-800"
+          descTextColor="text-purple-800/60"
+          borderClass="border-2 border-purple-800/60 shadow-purple-800/20"
         />
         <Feature
           title="Turn Visitors Into Customers"
           desc="User-focused design and strategy that turn visitors into loyal customers and drive real results."
           Icon={FaShoppingCart}
+          iconColor="text-green-500"
+          className="bg-green-100 text-green-800"
+          descTextColor="text-green-800/60"
+          borderClass="border-2 border-green-800/60 shadow-purple-800/20"
         />
       </div>
     </section>
