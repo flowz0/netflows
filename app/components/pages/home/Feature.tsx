663240a015f3484fe06497daeb2df9b2472af6d3
flowz0 <billylflowers@gmail.com>
2025-05-10 12:16:43 -0700

    fix: increase margin-top for title and description in Feature component

--- app/components/pages/home/Feature.tsx
+++ app/components/pages/home/Feature.tsx
@@ -14,10 +14,10 @@ export default function Feature({ Icon, title, desc, className, iconColor }: Fea
       <div className="border border-[hsl(0,3%,24%)] rounded-xl w-fit p-3">
         <Icon className={`w-6 h-6 ${iconColor}`} />
       </div>
-      <h3 className="mt-5 text-xl font-semibold md:text-2xl/[2rem]">
+      <h3 className="mt-6 text-xl font-semibold md:text-2xl/[2rem]">
         {title}
       </h3>
-      <p className="text-[#EFEFEF]/60 mt-2 leading-[1.8rem]">
+      <p className="text-[#EFEFEF]/60 mt-3 leading-[1.8rem]">
         {desc}
       </p>
     </div>
