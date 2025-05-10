55042f67cef340ef68485c260eca81db4da50e2a
flowz0 <billylflowers@gmail.com>
2025-05-10 12:14:35 -0700

    fix: set standard section padding & highlight keyword in title

--- app/components/pages/home/Hero.tsx
+++ app/components/pages/home/Hero.tsx
@@ -5,11 +5,15 @@ import HeroImg from "@/public/hero-img.svg";
 
 export default function Hero() {
   return (
-    <header className="pt-32 pb-48">
+    <header className="py-32">
       <div className="max-w-7xl mx-auto px-6 flex flex-col justify-between gap-8 items-center lg:flex-row">
         <div className="max-w-xl">
-          <h1 className="text-[#252323] text-4xl/[2.466rem] font-bold md:text-5xl/[3.416rem]">We Build Websites That Grow Your Business</h1>
-          <p className="text-[rgba(37,35,35,0.8)] text-base/[2.224rem] mt-4 md:text-lg/[2.274rem]">Strategic web design and development that turns visitors into customers.</p>
+          <h1 className="text-[#252323] text-4xl/[2.466rem] font-bold md:text-5xl/[3.416rem]">
+            We Build Websites That <span className="text-[#00A5CF]">Grow</span> Your Business
+          </h1>
+          <p className="text-[rgba(37,35,35,0.8)] text-base/[2.224rem] mt-4 md:text-lg/[2.274rem]">
+            Strategic web design and development that turns visitors into customers.
+          </p>
           <Button className="mt-9 w-full lg:w-fit">Book a Free Consultation</Button>
         </div>
         <Image
