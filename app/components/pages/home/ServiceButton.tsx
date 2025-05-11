c36aff3f6f78f7bf96d2769049b9ad06d53e3422
flowz0 <billylflowers@gmail.com>
2025-05-10 23:15:46 -0700

    feat(ui): add arrow icon to ServiceButton for improved UX & implement gradient background

--- app/components/pages/home/ServiceButton.tsx
+++ app/components/pages/home/ServiceButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ServiceType } from "@/app/types/service";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 type Props = {
   service: ServiceType;
@@ -12,11 +13,14 @@ function ServiceButton({ service, isActive, onClick }: Props) {
   return (
     <button
       onClick={onClick}
-      className={`text-xl/[1.466rem] font-bold p-4 rounded-xl flex transition-colors md:px-5 md:py-2 md:text-2xl/[2.649rem] ${
-        isActive ? "bg-[#00A5CF] text-[#EFEFEF] cursor-not-allowed" : "bg-[#CDCACC] text-[#252323] cursor-pointer hover:bg-[hsl(320,3%,70%)]"
+      className={`text-xl/[1.466rem] font-bold p-4 rounded-xl flex items-center gap-x-2 transition-colors md:px-5 md:py-2 md:text-2xl/[2.649rem] ${
+        isActive
+        ? "bg-linear-to-r from-[#00A5CF] to-[#00cfcf] text-[#EFEFEF] cursor-not-allowed"
+        : "bg-[#CDCACC] text-[#252323] cursor-pointer hover:bg-[hsl(320,3%,70%)]"
       }`}
     >
       {service.title}
+      <MdKeyboardArrowRight className="w-6 h-6" />
     </button>
   )
 }
