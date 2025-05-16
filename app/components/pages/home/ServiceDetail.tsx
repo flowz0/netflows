14d18bfcc1c129b73e4bccacf6000df857c3205d
flowz0 <billylflowers@gmail.com>
2025-05-15 20:15:48 -0700

    fix: optimize ServiceDetail component architecture & style customization

--- app/components/pages/home/ServiceDetail.tsx
+++ app/components/pages/home/ServiceDetail.tsx
@@ -9,20 +9,22 @@ type Props = {
 
 function ServiceDetail({ service }: Props) {
   return (
-    <div className="bg-[#252323] p-6 rounded-xl shadow shadow-[#252323] md:px-8 md:py-6 lg:w-3/5">
-      {/* Active Service Content */}
-      <h4 className="text-[#EFEFEF] text-3xl/[2.491rem] font-bold md:text-4xl/[3.399rem]">
-        {service.title}
-      </h4>
-      <p className="text-[rgba(239,239,239,0.8)] leading-[2.124rem] md:text-base/[2.224rem] mt-3 md:mt-2">
-        {service.description}
-      </p>
+    <div className="bg-[#242424] py-12 px-6 rounded-xl md:px-8 md:py-6 lg:w-3/5">
       <div className="flex justify-center">
         <Image
           src={service.image}
           alt={`${service.title} image`}
-          className="object-cover h-96 w-auto pointer-events-none mt-8"
+          className="object-cover h-96 w-auto pointer-events-none"
         />
+      </div>
+      {/* Active Service Content */}
+      <div className="flex flex-col items-center">
+        <h4 className="text-[#f5f5f5] text-3xl/[1.8rem] font-bold mt-8 text-center md:text-4xl/[3rem]">
+          {service.title}
+        </h4>
+        <p className="text-[#a8a8a8] text-base/[1.6rem] md:text-base/[1.8rem] mt-4 text-center max-w-xl md:mt-2">
+          {service.description}
+        </p>
       </div>
     </div>
   );
