e47ee9da8f49a39253ee6b11211913eb98a90ecf
flowz0 <billylflowers@gmail.com>
2025-05-17 17:17:47 -0700

    WIP: temp commit to continue on laptop

---
+++ app/components/pages/home/Testimonials.tsx
@@ -0,0 +1,21 @@
+export default function Testimonials() {
+  return (
+    <section className="py-32">
+      <div className="max-w-7xl mx-auto px-6">
+        <div>
+          <h1 className="text-[#f5f5f5] text-4xl/[2.4rem] font-bold text-center lg:text-start md:text-5xl/[3rem]">
+            What our clients
+            <br />
+            are <span className="text-[#00b4ff]">saying</span> about
+            <br />
+            working with us
+          </h1>
+        </div>
+
+        <div>
+        {/* Testimonials list */}
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Features from "./components/pages/home/Features";
 import Hero from "./components/pages/home/Hero";
+import Features from "./components/pages/home/Features";
 import Services from "./components/pages/home/Services";
+import Testimonials from "./components/pages/home/Testimonials";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Features />
       <Services />
+      <Testimonials />
     </>
   );
 };
