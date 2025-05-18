2eb9267e9e46034132186abe2e52cc711278685a
flowz0 <billylflowers@gmail.com>
2025-05-17 22:39:05 -0700

    feat: create functional testimonal selector component to improve UX/UI & credibility

---
+++ app/components/pages/home/Testimonial.tsx
@@ -0,0 +1,47 @@
+import Image, { StaticImageData } from "next/image";
+
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+
+import PlaceholderImg from "@/public/2000x2000.svg";
+
+type TestimonialProps = {
+  review: string;
+  reviewerImg?: StaticImageData;
+  reviewerName: string;
+  reviewerTitle: string;
+};
+
+export default function Testimonial({
+  review,
+  reviewerImg,
+  reviewerName,
+  reviewerTitle,
+}: TestimonialProps) {
+  return (
+    <div className="bg-[hsl(0,0%,14%)] px-7 py-8 rounded-lg md:max-w-2/3">
+      <div className="text-amber-300 flex gap-1">
+        <FaStar className="w-6 h-6" />
+        <FaStar className="w-6 h-6" />
+        <FaStar className="w-6 h-6" />
+        <FaStar className="w-6 h-6" />
+        <FaStarHalfAlt className="w-6 h-6" />
+      </div>
+      <p className="text-[#f5f5f5] text-base/[1.8rem] mt-6 max-w-2xl lg:mx-0 md:text-lg/[1.8rem]">
+        {review}
+      </p>
+      <div className="flex items-start gap-5 mt-6">
+        <Image
+          src={reviewerImg || PlaceholderImg}
+          alt="Testimonial Image"
+          className="rounded-full w-12 h-12"
+        />
+        <div>
+          <h3 className="text-[#f5f5f5] max-w-2xl mx-auto lg:text-start lg:mx-0">
+            {reviewerName}
+          </h3>
+          <h4 className="text-[#a8a8a8]">{reviewerTitle}</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/components/pages/home/Testimonials.tsx
+++ app/components/pages/home/Testimonials.tsx
@@ -1,19 +1,95 @@
+"use client";
+
+import { useState } from "react";
+import Testimonial from "./Testimonial";
+
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+const TestimonialData = [
+  {
+    id: 0,
+    review:
+      "“We had no online presence before partnering with the team. They built a professional, industry-focused website that led to a clear increase in customer inquiries. The process was smooth and efficient from start to finish.”",
+    // reviewerImg: "/path/to/image.jpg",
+    reviewerName: "Karson Kolle",
+    reviewerTitle: "Founder at Duct Daddy",
+  },
+  {
+    id: 1,
+    review:
+      "“Working with this team was a game-changer for our business. They took the time to understand our needs and delivered a website that not only looks great but also performs exceptionally well. Highly recommend!”",
+    reviewerName: "John Doe",
+    reviewerTitle: "CEO at Tech Innovations",
+  },
+  {
+    id: 2,
+    review:
+      "“From the initial consultation to the final launch, the team was professional and attentive. They transformed our outdated website into a modern, user-friendly platform that has received rave reviews from our customers.”",
+    reviewerName: "Jane Smith",
+    reviewerTitle: "Marketing Director at Creative Solutions",
+  },
+];
+
 export default function Testimonials() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextIndex = () => {
+    if (currentIndex < TestimonialData.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const prevIndex = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const currentTestimonial = TestimonialData[currentIndex];
+
   return (
     <section className="py-32">
       <div className="max-w-7xl mx-auto px-6">
-        <div>
-          <h1 className="text-[#f5f5f5] text-4xl/[2.4rem] font-bold text-center lg:text-start md:text-5xl/[3rem]">
-            What our clients
-            <br />
-            are <span className="text-[#00b4ff]">saying</span> about
-            <br />
+        <div className="flex flex-col justify-between gap-8 lg:items-end lg:flex-row">
+          <h1 className="text-[#f5f5f5] text-4xl/[2.4rem] font-bold lg:text-start md:text-5xl">
+            What our clients <br className="" />
+            are <span className="text-[#00b4ff]">saying</span> about{" "}
+            <br className="" />
             working with us
           </h1>
+          <div className="flex items-center justify-between sm:justify-end gap-6">
+            <p className="text-[#8f8f8f]">{currentTestimonial.id + 1}/{TestimonialData.length}</p>
+            <div className="flex justify-end gap-3">
+              <button onClick={prevIndex}>
+                <FaArrowLeft
+                  className={`bg-[hsl(0,0%,14%)] p-2 rounded-full w-8 h-8 ${
+                    currentIndex === 0
+                      ? "text-[hsl(0,0%,56%)] cursor-not-allowed"
+                      : "cursor-pointer hover:bg-[hsl(0,0%,24%)]"
+                  }`}
+                />
+              </button>
+              <button onClick={nextIndex}>
+                <FaArrowRight
+                  className={`bg-[hsl(0,0%,14%)] p-2 rounded-full w-8 h-8 ${
+                    currentIndex === TestimonialData.length - 1
+                      ? "text-[hsl(0,0%,56%)] cursor-not-allowed"
+                      : "cursor-pointer hover:bg-[hsl(0,0%,24%)]"
+                  }`}
+                />
+              </button>
+            </div>
+          </div>
         </div>
 
-        <div>
-        {/* Testimonials list */}
+        <div className="flex justify-end mt-8">
+          <Testimonial
+            key={currentTestimonial.id}
+            review={currentTestimonial.review}
+            // reviewerImg={currentTestimonial.reviewerImg}
+            reviewerName={currentTestimonial.reviewerName}
+            reviewerTitle={currentTestimonial.reviewerTitle}
+          />
         </div>
       </div>
     </section>
