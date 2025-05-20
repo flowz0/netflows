79aa22bc44cee53d27a8c259f5dbb008aece6795
flowz0 <billylflowers@gmail.com>
2025-05-19 21:01:30 -0700

    fix: remove iconColor & shadowColor props from Feature component, optimize SEO for Features component

--- app/components/pages/home/Feature.tsx
+++ app/components/pages/home/Feature.tsx
@@ -5,15 +5,13 @@ interface FeatureProps {
   title: string;
   desc: string;
   className?: string;
-  iconColor?: string;
-  shadowColor?: string;
 }
 
-export default function Feature({ Icon, title, desc, className, iconColor, shadowColor = "shadow-[#f5f5f5]" }: FeatureProps) {
+export default function Feature({ Icon, title, desc, className }: FeatureProps) {
   return (
     <div className={`${className} bg-[#242424] shadow-xs shadow-[#242424] rounded-lg w-full py-8 px-6`}>
-      <div className={`${shadowColor} shadow-xs rounded-full w-fit p-3`}>
-        <Icon className={`w-4 h-4 ${iconColor}`} />
+      <div className="bg-[#00b4ff] shadow-xs rounded-lg w-fit p-3">
+        <Icon className="text-[#0a0a0a] w-4 h-4" />
       </div>
       <h3 className="mt-8 text-xl font-semibold md:text-2xl/[1.8rem]">
         {title}
--- app/components/pages/home/Features.tsx
+++ app/components/pages/home/Features.tsx
@@ -7,32 +7,24 @@ export default function Features() {
     <section>
       <div className="max-w-7xl pb-32 px-6 mx-auto grid lg:justify-evenly gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <Feature
-          title="Custom Website Development"
-          desc="Get a custom, responsive website designed to drive traffic and conversions, tailored to your business needs."
+          title="Custom Web Development"
+          desc="Get a custom, responsive website/app designed to drive traffic and conversions, tailored to your business needs."
           Icon={FaCode}
-          iconColor="text-sky-400"
-          shadowColor="shadow-sky-400"
         />
         <Feature
-          title="User-Focused Web Design"
+          title="User-Centric Web Design"
           desc="We create intuitive, modern designs that enhance user experience and boost engagement across all devices."
           Icon={FaPaintBrush}
-          iconColor="text-purple-500"
-          shadowColor="shadow-purple-500"
         />
         <Feature
           title="Ongoing Website Maintenance"
           desc="Ensure your site runs smoothly with regular updates, backups, and performance optimizations so you can focus on your business."
           Icon={FaWrench}
-          iconColor="text-orange-500"
-          shadowColor="shadow-orange-500"
         />
         <Feature
-          title="SEO & Mobile Optimizations"
+          title="Mobile & SEO Optimizations"
           desc="Boost your site&apos;s Google ranking and optimize for mobile, ensuring fast, accessible experiences for all users."
           Icon={FaSearchDollar}
-          iconColor="text-green-500"
-          shadowColor="shadow-green-500"
         />
       </div>
     </section>
