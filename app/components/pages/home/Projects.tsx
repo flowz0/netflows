a7f18de82ab8bd3356618235a68ca5892c8adbdf
flowz0 <billylflowers@gmail.com>
2025-05-19 23:18:52 -0700

    fix: optimize Projects component structure and increase line-height css

--- app/components/pages/home/Projects.tsx
+++ app/components/pages/home/Projects.tsx
@@ -6,8 +6,10 @@ export default function Projects() {
     <section className="py-32">
       <div className="max-w-7xl mx-auto px-6">
         <div>
-          <h2 className="text-[#f5f5f5] text-4xl/[2.4rem] font-semibold text-center lg:text-start md:text-5xl/[3rem]">
-            What We&apos;ve <span className="text-[#00b4ff] font-bold">Built</span>
+          <h2 className="text-[#f5f5f5] text-4xl/[2.6rem] font-semibold text-center lg:text-start md:text-5xl/[3.4rem]">
+            What We&apos;ve 
+            <br />
+            <span className="text-[#00b4ff] font-bold">Built</span>
           </h2>
           <p className="text-[#a8a8a8] text-base/[1.8rem] mt-4 max-w-2xl mx-auto text-center lg:text-start lg:mx-0 md:text-lg/[2rem]">
             Explore the web design and development solutions we&apos;ve crafted for clients across industries.
