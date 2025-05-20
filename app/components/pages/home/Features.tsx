26849f7df312d849bf4834dc007f574459406fb1
flowz0 <billylflowers@gmail.com>
2025-05-20 15:35:54 -0700

    fix: optimize Features component SEO

--- app/components/pages/home/Features.tsx
+++ app/components/pages/home/Features.tsx
@@ -7,22 +7,22 @@ export default function Features() {
     <section>
       <div className="max-w-7xl pb-32 px-6 mx-auto grid lg:justify-evenly gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <Feature
-          title="Custom Web Development"
+          title="Web Development"
           desc="Get a custom, responsive website/app designed to drive traffic and conversions, tailored to your business needs."
           Icon={FaCode}
         />
         <Feature
-          title="User-Centric Web Design"
+          title="Web Design"
           desc="We create intuitive, modern designs that enhance user experience and boost engagement across all devices."
           Icon={FaPaintBrush}
         />
         <Feature
-          title="Ongoing Website Maintenance"
+          title="Website Maintenance"
           desc="Ensure your site runs smoothly with regular updates, backups, and performance optimizations so you can focus on your business."
           Icon={FaWrench}
         />
         <Feature
-          title="Mobile & SEO Optimizations"
+          title="SEO Optimizations"
           desc="Boost your site&apos;s Google ranking and optimize for mobile, ensuring fast, accessible experiences for all users."
           Icon={FaSearchDollar}
         />
