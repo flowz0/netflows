e14ec42bfc086af90152a42fd6438d2200d72ea7
flowz0 <billylflowers@gmail.com>
2025-05-24 10:09:17 -0700

    fix: add faq id to FAQs component

--- app/components/pages/home/FAQs.tsx
+++ app/components/pages/home/FAQs.tsx
@@ -8,7 +8,7 @@ export default function FAQs() {
   const rightColumn = faqData.filter((_, index) => index % 2 === 1);
 
   return (
-    <section className="py-32 px-6 max-w-7xl mx-auto">
+    <section id="faq" className="py-32 px-6 max-w-7xl mx-auto">
       <div className="flex flex-col items-center">
         <p className="bg-[#00b3ff] text-[#0a0a0a] font-bold py-1 px-5 rounded-full">FAQs</p>
         <h2 className="text-[#f5f5f5] mt-4 text-4xl/[2.6rem] font-semibold text-center md:text-5xl/[3.4rem]">
