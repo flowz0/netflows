beece822d70b5596d5ebe3cf4c445621a9da2a33
flowz0 <billylflowers@gmail.com>
2025-05-27 21:04:32 -0700

    fix(pages): implement Testimonials component in Projects layout

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Testimonials from "../components/pages/home/Testimonials";
 
 export const metadata: Metadata = {
   title: "Case Studies | Netflows",
@@ -13,6 +14,7 @@ export default function ProjectLayout({
   return (
     <>
       {children}
+      <Testimonials />
     </>
   );
 }
