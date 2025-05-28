76ee1c60b6b404ce8bb7ac9032596a6b22d9eb72
flowz0 <billylflowers@gmail.com>
2025-05-27 20:42:17 -0700

    feat(components): create CopyEmailBtn component to improve UI/UX

---
+++ app/components/CopyEmailBtn.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
+export default function CopyEmailBtn() {
+  const [copied, setCopied] = useState(false);
+  const email = "netflowsxyz@gmail.com"
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(email);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000) // Reset copied after 2s
+    } catch (error) {
+      console.error("Failed to copy email:", error);
+    }
+  };
+
+  return (
+    <div className="mt-4 flex items-center gap-4">
+      <p id="emailToCopy" className="text-[#f5f5f5] text-base/[1.8rem] font-semibold">
+        {email}
+      </p>
+      <button
+        type="button"
+        onClick={handleCopy}
+        className="bg-[#242424] text-[#f5f5f5] px-6 py-1 rounded-full transition-all cursor-pointer font-semibold text-sm/[30px] tracking-[0.016em] hover:bg-[hsl(0,0%,24%)] active:bg-[hsl(0,0%,34%)]"
+      >
+        {copied ? "Email Copied" : "Copy Email"}
+      </button>
+    </div>
+  );
+}
