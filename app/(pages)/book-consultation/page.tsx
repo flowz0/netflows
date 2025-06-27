2c189ea093e36b8f11b2ca716df17f9a7ee30e00
flowz0 <billylflowers@gmail.com>
2025-06-26 19:35:27 -0700

    feat: create BookConsultationPage form UI

--- app/(pages)/book-consultation/page.tsx
+++ app/(pages)/book-consultation/page.tsx
@@ -11,6 +11,57 @@ export default function BookConsultationPage() {
           Let&apos;s explore your vision, define your goals, and lay the groundwork for a custom web solution that drives results.
         </p>
       </header>
+      <div className="max-w-2xl mx-auto">
+        <form className="bg-[#242424] text-[#f5f5f5] mt-12 py-14 px-14 rounded-2xl shadow-xs shadow-[#242424]">
+          <div className="flex flex-col">
+            <label htmlFor="name">Full Name</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="John Doe"
+              className="mt-2 bg-[hsl(0,0%,20%)] py-3 px-5 rounded-lg active:ring-0  focus:outline-none active:outline-none focus:border-none active:border-none placeholder:text-[hsl(0,0%,60%)] focus:ring-2 focus:ring-[#0080DB]"
+            />
+          </div>
+          <div className="mt-4 flex flex-col">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="example@email.com"
+              className="mt-2 bg-[hsl(0,0%,20%)] py-3 px-5 rounded-lg active:ring-0  focus:outline-none active:outline-none focus:border-none active:border-none placeholder:text-[hsl(0,0%,60%)] focus:ring-2 focus:ring-[#0080DB]"
+            />
+          </div>
+          <div className="mt-4 flex flex-col">
+            <label htmlFor="phone">Phone number</label>
+            <input
+              id="phone"
+              name="phone"
+              type="tel"
+              placeholder="(555) 555-5555"
+              className="mt-2 bg-[hsl(0,0%,20%)] py-3 px-5 rounded-lg active:ring-0  focus:outline-none active:outline-none focus:border-none active:border-none placeholder:text-[hsl(0,0%,60%)] focus:ring-2 focus:ring-[#0080DB]"
+            />
+          </div>
+          <div className="mt-4 flex flex-col">
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              placeholder="(555) 555-5555"
+              className="mt-2 bg-[hsl(0,0%,20%)] py-3 px-5 rounded-lg active:ring-0  focus:outline-none active:outline-none focus:border-none active:border-none placeholder:text-[hsl(0,0%,60%)] focus:ring-2 focus:ring-[#0080DB]"
+            />
+          </div>
+          <button 
+            type="button"
+            className="mt-6 px-7 py-2 w-full rounded-full cursor-pointer font-bold text-base/[30px] tracking-[0.016em] bg-[#00b4ff] text-[#0a0a0a] duration-500 transition-colors hover:bg-[hsl(198,100%,40%)] active:bg-[hsl(198,100%,30%)]"
+          >
+            Schedule Call
+          </button>
+          <p className="mt-4 text-sm opacity-60">Add service option menu and date picker</p>
+        </form>
+      </div>
     </main>
   );
 }
