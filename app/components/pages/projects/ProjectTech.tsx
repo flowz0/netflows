903ed74cb5957c9ecbb64b46e221f2f3f5c51706
flowz0 <billylflowers@gmail.com>
2025-07-03 00:57:22 -0700

    fix: update prop types and interpolating iconColor to inline style in ProjectTech component

--- app/components/pages/projects/ProjectTech.tsx
+++ app/components/pages/projects/ProjectTech.tsx
@@ -1,15 +1,15 @@
-import { ElementType } from "react";
-
-interface TechProps {
-  Icon: ElementType;
+export default function ProjectTech({
+  Icon,
+  name,
+  iconColor
+}: {
+  Icon: React.ElementType;
   name: string;
-  iconColor: string;
-}
-
-export default function ProjectTech({ Icon, name, iconColor }: TechProps) {
+  iconColor: string
+}) {
   return (
     <li className="bg-[hsl(0,0%,14%)] flex items-center gap-3 py-2 px-3 rounded-lg text-sm">
-      <Icon className={`text-[#${iconColor}] w-5 h-5`} />
+      <Icon className="w-5 h-5" style={{ color: `${iconColor}` }} />
       <p>
         {name}
       </p>
