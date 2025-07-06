83cf00ffd51e07d50b12ff1665de91cd4c6fbc48
flowz0 <billylflowers@gmail.com>
2025-07-06 02:30:38 -0700

    chore(form): refactor Step1 into a separate component for clarity and reuse

--- app/components/pages/book-consultation/multi-step-form/MultiStepForm.tsx
+++ app/components/pages/book-consultation/multi-step-form/MultiStepForm.tsx
@@ -8,6 +8,7 @@ import Step4 from "./Step4";
 import Select from "./Select";
 import PhoneNumber from "./PhoneNumber";
 import { FaUser, FaCalendarAlt, FaClock, FaCheckCircle } from "react-icons/fa";
+import Step1 from "./Step1";
 
 const steps = [
   { id: 1, label: "Info", icon: FaUser },
@@ -18,7 +19,6 @@ const steps = [
 
 export default function MultiStepForm() {
   const [step, setStep] = useState(1);
-  const [selectedService, setSelectedService] = useState("");
   const [formData, setFormData] = useState<FormData>({
     name: "",
     email: "",
@@ -71,77 +71,13 @@ export default function MultiStepForm() {
         })}
       </div>
 
-      {step === 1 && (
-        <div className="flex flex-col mt-8 sm:mt-12">
-          <div className="flex flex-col gap-y-4">
-            <div className="flex flex-col">
-              <label htmlFor="name" className="text-[hsl(0,0%,80%)] text-sm">Full name</label>
-              <input
-                type="text"
-                id="name"
-                placeholder="John Doe"
-                value={formData.name}
-                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                className="mt-2 bg-[hsl(0,0%,20%)] py-3 px-5 rounded-lg active:ring-0 focus:outline-none active:outline-none focus:border-none active:border-none placeholder:text-[hsl(0,0%,60%)] focus:ring-2 focus:ring-[#0080DB]"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="email" className="text-[hsl(0,0%,80%)] text-sm">Email</label>
-              <input
-                type="email"
-                id="email"
-                placeholder="example@email.com"
-                value={formData.email}
-                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                className="mt-2 bg-[hsl(0,0%,20%)] py-3 px-5 rounded-lg active:ring-0 focus:outline-none active:outline-none focus:border-none active:border-none placeholder:text-[hsl(0,0%,60%)] focus:ring-2 focus:ring-[#0080DB]"
-              />
-            </div>
-            <PhoneNumber
-              value={formData.phone}
-              onChange={(formatted) => setFormData({ ...formData, phone: formatted })}
-            />
-            <div className="flex flex-col">
-              <Select
-                label="Service"
-                selected={selectedService}
-                onChange={(value) => {
-                  setSelectedService(value);
-                  setFormData((prev) => ({ ...prev, service: value }));
-                }}
-                options={[
-                  { value: "", label: "Choose a service" },
-                  { value: "web development", label: "Web Development" },
-                  { value: "web design", label: "Web Design" },
-                  { value: "maintenance & support", label: "Maintenance & Support" },
-                  { value: "web hosting", label: "Web Hosting" },
-                  { value: "domain name", label: "Domain Name" },
-                  { value: "SEO optimization", label: "SEO Optimization" },
-                  { value: "performance optimization", label: "Performance Optimization" },
-                  { value: "additional pages", label: "Additional Pages" },
-                  { value: "3rd party api integration", label: "3rd Party API Integration" },
-                ]}
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="message" className="text-[hsl(0,0%,80%)] text-sm">Message</label>
-              <textarea
-                id="message"
-                placeholder="Aditional information"
-                value={formData.message}
-                rows={4}
-                onChange={(e) => setFormData({ ...formData, message: e.target.value })}
-                className="mt-2 bg-[hsl(0,0%,20%)] py-3 px-5 rounded-lg resize-none active:ring-0 focus:outline-none active:outline-none focus:border-none active:border-none placeholder:text-[hsl(0,0%,60%)] focus:ring-2 focus:ring-[#0080DB]"
-              />
-            </div>
-          </div>
-          <button
-            onClick={nextStep}
-            className="bg-[hsl(198,100%,40%)] text-[hsl(0,0%,92%)] mt-4 w-fit font-semibold px-4 py-2 rounded-lg cursor-pointer hover:bg-[hsl(198,100%,48%)]"
-          >
-            Next
-          </button>
-        </div>
-      )}
+      {step === 1 &&
+        <Step1
+          formData={formData}
+          setFormData={setFormData}
+          nextStep={nextStep}
+        />
+      }
 
       {step === 2 &&
         <Step2
