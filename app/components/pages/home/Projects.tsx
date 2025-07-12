cafd3010d6a6ab2e2135d67c0f4b2dd51ad2b68e
flowz0 <billylflowers@gmail.com>
2025-07-11 17:10:12 -0700

    fix: show 2 cols only for desktop and center text until desktop in Projects component

--- app/components/pages/home/Projects.tsx
+++ app/components/pages/home/Projects.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
     <section className="pt-32 pb-16 sm:pb-32">
       <div className="max-w-7xl mx-auto px-6">
-        <div className="flex flex-col items-center md:items-start">
+        <div className="flex flex-col items-center lg:items-start">
           <h2 className="text-[#f5f5f5] text-4xl/[2.6rem] font-semibold text-center md:text-start md:text-5xl/[3.4rem]">
             What We&apos;ve <span className="text-[#00b4ff] font-bold">Built</span>
           </h2>
@@ -13,7 +13,7 @@ export default function Projects() {
             Explore the web design and development solutions we&apos;ve crafted for clients across industries.
           </p>
         </div>
-        <div className="mt-16 grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="mt-16 grid grid-cols-1 lg:grid-cols-2 gap-6">
           {ProjectData.slice(0, 4).map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
