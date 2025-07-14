c4ccb0af5218dcbb8aae5fdeb57a5f0b4c556525
flowz0 <billylflowers@gmail.com>
2025-07-13 22:08:46 -0700

    fix: optimize line-height in Hero component

--- app/components/pages/home/Hero.tsx
+++ app/components/pages/home/Hero.tsx
@@ -8,12 +8,12 @@ export default function Hero() {
     <header className="pt-14">
       <div className="max-w-7xl mt-8 mx-auto px-6 flex flex-col justify-between gap-12 items-center lg:flex-row">
         <div className="max-w-xl flex flex-col items-center lg:items-start">
-          <h1 className="text-[#f5f5f5] text-4xl/[2.466rem] font-semibold text-center lg:text-start md:text-5xl/[3.016rem]">
+          <h1 className="text-[#f5f5f5] text-4xl font-semibold text-center lg:text-start md:text-5xl">
             We <span className="text-[rgb(0,180,255)] font-bold">Build</span> Web Experiences That 
             <br className="hidden sm:inline-block" />
             {" "} Grow Your Business
           </h1>
-          <p className="text-[#a8a8a8] text-base/[2.024rem] mt-4 text-center lg:text-start md:text-lg/[2.274rem]">
+          <p className="text-[#a8a8a8] text-base mt-4 text-center lg:text-start md:text-lg">
             Strategic web design and development that turns visitors into customers.
           </p>
           <Link href="/book-consultation">
