ccd8bd6cdbcd93f89a867b699cab7e0f6946fd32
flowz0 <billylflowers@gmail.com>
2025-07-13 22:09:04 -0700

    fix optimize line-height & margin in Projects component

--- app/components/pages/home/Projects.tsx
+++ app/components/pages/home/Projects.tsx
@@ -6,14 +6,14 @@ export default function Projects() {
     <section className="pt-32 pb-16 sm:pb-32">
       <div className="max-w-7xl mx-auto px-6">
         <div className="flex flex-col items-center lg:items-start">
-          <h2 className="text-[#f5f5f5] text-4xl/[2.6rem] font-semibold text-center md:text-start md:text-5xl/[3.4rem]">
+          <h2 className="text-[#f5f5f5] text-4xl font-semibold text-center md:text-start md:text-5xl">
             What We&apos;ve <span className="text-[#00b4ff] font-bold">Built</span>
           </h2>
-          <p className="text-[#a8a8a8] text-base/[1.8rem] mt-4 max-w-md text-center md:text-start lg:mx-0 md:text-lg/[2rem]">
+          <p className="text-[#a8a8a8] text-base mt-4 max-w-md text-center md:text-start lg:mx-0 md:text-lg">
             Explore the web design and development solutions we&apos;ve crafted for clients across industries.
           </p>
         </div>
-        <div className="mt-16 grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
           {ProjectData.slice(0, 4).map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
