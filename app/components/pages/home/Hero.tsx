785475bd6b375940aa27b4aa7390ed7a4bd62efc
flowz0 <billylflowers@gmail.com>
2025-07-15 03:19:31 -0700

    fix: align Image properly in Hero component

--- app/components/pages/home/Hero.tsx
+++ app/components/pages/home/Hero.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <header className="pt-14">
-      <div className="max-w-7xl mt-8 mx-auto px-6 flex flex-col justify-between gap-12 items-center lg:flex-row">
+      <div className="max-w-7xl mt-8 mx-auto px-6 flex flex-col justify-between gap-12 items-center md:pr-0 lg:flex-row">
         <div className="max-w-xl flex flex-col items-center lg:items-start">
           <h1 className="text-[#f5f5f5] text-4xl font-semibold text-center lg:text-start md:text-5xl">
             We <span className="text-[rgb(0,180,255)] font-bold">Build</span> Web Experiences That 
