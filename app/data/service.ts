eb69f793d0d70540650188f27b9438da97890e34
flowz0 <billylflowers@gmail.com>
2025-07-21 20:50:57 -0700

    fix: create services and testimonials data

---
+++ app/data/service.ts
@@ -0,0 +1,21 @@
+import WebDevelopmentImg from "@/public/home/web-development-img.svg";
+import WebDesignImg from "@/public/home/web-design-img.svg";
+import MaintenanceImg from "@/public/home/website-maintenance-img.svg";
+
+export const services = [
+  {
+    title: "Development",
+    description: "We build scalable and performant websites and applications.",
+    image: WebDevelopmentImg,
+  },
+  {
+    title: "UI/UX Design",
+    description: "UI/UX design is designing digital interfaces for a great user experience.",
+    image: WebDesignImg,
+  },
+  {
+    title: "Maintenance",
+    description: "We provide ongoing support and maintenance for your digital products.",
+    image: MaintenanceImg,
+  },
+];
