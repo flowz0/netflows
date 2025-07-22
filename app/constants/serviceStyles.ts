4d7ce54de8dbf28dd802cf03d7aac85ea95a3eb7
flowz0 <billylflowers@gmail.com>
2025-07-21 20:51:07 -0700

    fix: create serviceStyles constant

---
+++ app/constants/serviceStyles.ts
@@ -0,0 +1,25 @@
+export const SERVICE_STYLES: Record<
+  string,
+  { title: string, bg: string; text: string }
+> = {
+  design: { 
+    title: "Design", 
+    bg: "bg-pink-100", 
+    text: "text-pink-800" 
+  },
+  development: { 
+    title: "Development", 
+    bg: "bg-blue-100", 
+    text: "text-blue-800" 
+  },
+  seo: { 
+    title: "SEO", 
+    bg: "bg-green-100", 
+    text: "text-green-800" 
+  },
+  maintenance: { 
+    title: "Maintenance", 
+    bg: "bg-yellow-100", 
+    text: "text-yellow-800" 
+  },
+};
