e1b3778fcb94c3d80695296be80e0c6060331e24
flowz0 <billylflowers@gmail.com>
2025-07-21 21:21:59 -0700

    fix: create Process v2 components

---
+++ app/components/Home/Process.tsx
@@ -0,0 +1,39 @@
+import ProcessCard from "./ProcessCard";
+import ProcessCTA from "./ProcessCTA";
+
+export default function Process() {
+  return (
+    <section className="my-64 max-w-7xl mx-auto">
+      <div className="flex items-end">
+        <h2 className="text-h2 font-bold font-nunito text-black w-4/6">
+          Our Process
+        </h2>
+        <div className="w-5/6 border-b border-black50"></div>
+      </div>
+
+      <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:gap-x-6 md:grid-cols-4">
+        <ProcessCard
+          step="01."
+          title="Discovery"
+          desc="Learn about your goals, audience, and vision to plan the design and development phases."
+        />
+        <ProcessCard
+          step="02."
+          title="Design"
+          desc="Create a clean, user-friendly design file that aligns with your brand's identity and goals."
+        />
+        <ProcessCard
+          step="03."
+          title="Development"
+          desc="Build responsive, fast, and secure web solutions with modern technologies."
+        />
+        <ProcessCard
+          step="04."
+          title="Launch"
+          desc="After QA, we launch your solution and offer ongoing support to keep it running smoothly."
+        />
+      </div>
+      <ProcessCTA />
+    </section>
+  );
+}
---
+++ app/components/Home/ProcessCTA.tsx
@@ -0,0 +1,34 @@
+import PlaceholderAvatar from "@/public/2000x2000.svg";
+import Image from "next/image";
+
+export default function ProcessCTA() {
+  return (
+    <div className="mt-6 border h-[128px] border-black py-8 px-8 rounded-2xl flex items-center justify-between">
+      <div className="flex items-center gap-x-4">
+        <div className="flex -space-x-4">
+          <Image
+            src={PlaceholderAvatar}
+            alt="Business avatar"
+            className="h-12 w-12 rounded-full ring-4 ring-black5"
+          />
+          <Image
+            src={PlaceholderAvatar}
+            alt="Business avatar"
+            className="h-12 w-12 rounded-full ring-4 ring-black5"
+          />
+          <Image
+            src={PlaceholderAvatar}
+            alt="Business avatar"
+            className="h-12 w-12 rounded-full ring-4 ring-black5"
+          />
+        </div>
+        <p className="text-black75 text-p font-inter">
+          Join businesses leveling up their online presence
+        </p>
+      </div>
+      <button className="py-4 px-8 text-p font-inter rounded-2xl cursor-pointer text-black5 bg-linear-to-r from-primary to-secondary">
+        Book Free Consultation
+      </button>
+    </div>
+  );
+}
