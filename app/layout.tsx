436b3c12bbf4c85a50c9abd3b3d2c24bf68c1782
flowz0 <billylflowers@gmail.com>
2025-09-17 19:51:09 -0700

    implement GoogleAnalytics into app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -100,15 +100,6 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MKV94BSM');`}
-        </Script>
-      </head>
       <body
         className={`${nunito.variable} ${inter.variable} bg-black5 text-black antialiased`}
       >
@@ -116,18 +107,11 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
         />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MKV94BSM"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Navbar />
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-N5FZZBFY28" />
     </html>
   );
 }
