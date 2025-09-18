cfb0373c7c09f7798feb81ecf0c4a0fd589d6bff
flowz0 <billylflowers@gmail.com>
2025-09-17 21:02:20 -0700

    fix(cta): improve conversions & SEO by updating component content

--- components/pages/home/CTA.tsx
+++ components/pages/home/CTA.tsx
@@ -3,16 +3,17 @@ import Link from "next/link";
 
 export default function CTA() {
   return (
-    <section className="mt-32 px-6 max-w-7xl mx-auto md:mt-64">
-      <div className="border border-black rounded-2xl p-8 sm:h-[400px] flex flex-col items-center justify-center cursor-default">
+    <section className="mt-32 px-6 max-w-7xl mx-auto relative md:mt-64">
+      <div className="border bg-black5 border-black rounded-2xl p-8 animate-drop-shadow sm:h-[400px] flex flex-col items-center justify-center cursor-default">
         <h3 className="text-h5 sm:text-h3 font-bold text-center font-nunito text-black">
-          Book a Consultation Today
+          Ready to <span className="text-primary">Elevate</span> Your <span className="text-secondary">Online</span> Presence?
         </h3>
         <p className="mt-4 text-p max-w-2xl text-center font-inter text-black75">
-          Schedule a free consultation with our industry expert software engineers and web designers to bring your vision to life.
+          Book a free 15-min strategy call with a Netflows expert today!
+          Let&apos;s discuss how we can design and develop a high-performing website tailored to your goals — no pressure, just real solutions.
         </p>
         <Link href="/booking" className="mt-8 md:mt-12">
-          <Button text="Book Free Consultation" variant="gradient" />
+          <Button text="Book My Free Call" variant="gradient" />
         </Link>
       </div>
     </section>
