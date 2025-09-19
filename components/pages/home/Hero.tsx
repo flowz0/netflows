0e51ed73fae11c6e55fee9e378bf57b15b20904a
flowz0 <billylflowers@gmail.com>
2025-09-18 23:23:10 -0700

    fix: improve Hero component content to optimize SEO

--- components/pages/home/Hero.tsx
+++ components/pages/home/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import HeroImg from "@/public/hero-img.png";
+import HeroImg from "@/public/projects/dddc-thumbnail.jpg";
 import Link from "next/link";
 import Button from "@/components/Button";
 
@@ -8,13 +8,13 @@ export default function Hero() {
   return (
     <section className="mt-8 max-w-7xl mx-auto lg:mt-16">
       <div className="flex flex-col items-center cursor-default px-6">
-        <h1 className="text-h3 font-bold font-nunito text-center sm:text-h1">
-          <span className="text-primary">Grow</span> Online. {" "}
+        <h1 className="text-h3 font-bold font-nunito text-center sm:text-h2">
+          <span className="text-secondary">Web Development</span> & <span className="text-primary">Design</span>
           <br />
-          Attract More <span className="text-secondary">Customers</span>.
+          That <span className="text-primary">Helps</span> Your <span className="text-secondary">Business</span> Grow
         </h1>
-        <p className="text-black75 text-p text-center font-inter max-w-xl mt-4">
-          We craft high-performing solutions that drive traffic, build trust, and help small businesses scale with ease.
+        <p className="text-black75 text-p text-center font-inter max-w-2xl mt-4">
+          At Netflows, we design and develop perfomant, responsive, and SEO-optimized websites that help small businesses and startups grow their online presence, generate more leads, and build long-term customer trust.
         </p>
         <Link href="/booking" className="mt-8 md:mt-12">
           <Button text="Get My Free Consultation" variant="gradient" />
