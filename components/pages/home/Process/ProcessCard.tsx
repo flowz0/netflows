ec507c7e1551ca578163bc40d7ced71333ddc2bc
flowz0 <billylflowers@gmail.com>
2025-09-18 23:19:24 -0700

    fix: improve ProcessCard component responsive height and spacing

--- components/pages/home/Process/ProcessCard.tsx
+++ components/pages/home/Process/ProcessCard.tsx
@@ -10,13 +10,16 @@ export default function ProcessCard({
   desc
 }: ProcessCardProps) {
   return (
-    <div className="border h-[320px] lg:h-[440px] border-black py-8 px-8 rounded-2xl flex flex-col justify-between cursor-default">
-      <h6 className="text-black text-h6 font-bold font-nunito">
-        {step}
-        <br />
-        {title}
-      </h6>
-      <p className="text-black75 text-p font-inter">
+    <div className="border min-h-[340px] lg:min-h-[500px] border-black py-8 px-8 rounded-2xl flex flex-col justify-between cursor-default">
+      <div className="text-black font-bold font-nunito">
+        <h5 className="text-h4">
+          {step}
+        </h5>
+        <h6 className="text-h6 mt-4">
+          {title}
+        </h6>
+      </div>
+      <p className="text-black75 text-p mt-8 font-inter">
         {desc}
       </p>
     </div>
