6dbc01d9bf5733fffab22f895c1ecd1e15105cfa
flowz0 <billylflowers@gmail.com>
2025-09-19 21:17:54 -0700

    fix: improve padding and color for ProjectTech component

--- components/pages/projects/ProjectTech.tsx
+++ components/pages/projects/ProjectTech.tsx
@@ -8,7 +8,7 @@ export default function ProjectTech({
   iconColor: string
 }) {
   return (
-    <li className="bg-black75 text-black5 text-small font-inter flex items-center gap-3 py-3 px-4 rounded-lg">
+    <li className="bg-black10/60 text-black75 text-small font-inter flex items-center gap-3 py-2 px-3 rounded-lg">
       <Icon className="w-6 h-6" style={{ color: `${iconColor}` }} />
       <p>
         {name}
