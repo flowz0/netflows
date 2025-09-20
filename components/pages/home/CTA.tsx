1034e51aa1d28f81852304953a215b3e779f084a
flowz0 <billylflowers@gmail.com>
2025-09-19 20:24:52 -0700

    fix: decrease mobile padding for CTA component

--- components/pages/home/CTA.tsx
+++ components/pages/home/CTA.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function CTA() {
   return (
     <section className="mt-32 px-6 max-w-7xl mx-auto relative md:mt-64">
-      <div className="border bg-black5 border-black rounded-2xl p-8 animate-drop-shadow sm:h-[400px] flex flex-col items-center justify-center cursor-default">
+      <div className="border bg-black5 border-black rounded-2xl p-6 sm:p-8 animate-drop-shadow sm:h-[400px] flex flex-col items-center justify-center cursor-default">
         <h3 className="text-h5 sm:text-h3 font-bold text-center font-nunito text-black">
           Ready to <span className="text-primary">Elevate</span> Your <span className="text-secondary">Online</span> Presence?
         </h3>
